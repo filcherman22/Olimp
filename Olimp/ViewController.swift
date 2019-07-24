257808197452ff0ef7e9b4d181dd841235fe6817
Filipp <filipp.romano@gmail.com>
2019-07-24 17:41:58 +0300

    Initial Commit

---
+++ Olimp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Olimp
+//
+//  Created by Филипп on 24.07.2019.
+//  Copyright © 2019 Филипп. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
